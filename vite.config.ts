bd9221921ac094544ce6cfde78b9ddc6f4fd8d30
pktherock <59223750+pktherock@users.noreply.github.com>
2024-06-18 00:52:07 +0530

    Install important packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    TanStackRouterVite(),
     checker({
       eslint: {
         // for example, lint .ts and .tsx
